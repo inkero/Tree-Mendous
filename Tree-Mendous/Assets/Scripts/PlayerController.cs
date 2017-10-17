4ddd689310bae1e78d94b1d5b0a9c539d23ffd1b
Jussi Inkeroinen <inkeroinenjussi@gmail.com>
2017-10-18 01:24:49 +0300

    Added my first code to the custom PlayerController script, now the character moves with animation and flips around when changing directions. The camera also adjusts accordingly now.

--- Tree-Mendous/Assets/Scripts/PlayerController.cs
+++ Tree-Mendous/Assets/Scripts/PlayerController.cs
@@ -7,13 +7,39 @@ public class PlayerController : MonoBehaviour {
 	// Movement variables
 	public float maxSpeed;
 
+	Rigidbody2D myRB;
+	Animator myAnim;
+	bool facingRight;
+
 	// Use this for initialization
 	void Start () {
-		
+		myRB = GetComponent<Rigidbody2D> ();
+		myAnim = GetComponent<Animator> ();
+
+		facingRight = true;
 	}
 	
-	// Update is called once per frame
-	void Update () {
-		
+	// Update is called every period of time
+	void FixedUpdate () {
+		float move = Input.GetAxis ("Horizontal");
+		myAnim.SetFloat ("speed", Mathf.Abs(move));
+
+		// Move the character
+		myRB.velocity = new Vector2 (move * maxSpeed, myRB.velocity.y);
+
+		// If we move right & the character is not facing right, flip
+		if(move>0 && !facingRight){
+			flip ();
+		// And if we move left & the character is facing right, flip
+		} else if(move<0 && facingRight) {
+			flip ();
+		}
+	}
+
+	void flip(){
+		facingRight = !facingRight;
+		Vector3 theScale = transform.localScale;
+		theScale.x *= -1; // Same as: theScale.x = theScale.x * -1
+		transform.localScale = theScale;
 	}
 }
