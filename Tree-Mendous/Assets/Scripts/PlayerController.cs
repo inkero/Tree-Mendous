373860380144585b12860c80b9bc81abda8d18d0
Jussi Inkeroinen <inkeroinenjussi@gmail.com>
2017-10-18 00:41:24 +0300

    Started work on custom character prefab, made another test animation, added custom animation controller, started some custom code for the test character

---
+++ Tree-Mendous/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Movement variables
+	public float maxSpeed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
