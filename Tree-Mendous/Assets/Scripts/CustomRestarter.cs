9ee8aaca0c8c076c49122afa0c8475ab32b1357d
Frans <Frans.auranen@edu.turkuamk.fi>
2017-12-06 21:20:50 +0200

    UI + respawn changes
    alot of stuff

--- Tree-Mendous/Assets/Scripts/CustomRestarter.cs
+++ Tree-Mendous/Assets/Scripts/CustomRestarter.cs
@@ -7,24 +7,48 @@ public class CustomRestarter : MonoBehaviour {
 	public float respawnTime = 2.0f;
 	public GameObject player;
 	public Transform spawnPoint;
+    public PlayerController playerController;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    bool soundPlayed = false;
+
+    // Use this for initialization
+    void Start () {
+        playerController = player.GetComponent<PlayerController>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            StartCoroutine(Restart());
+        }
+    }
 
 
 
-	IEnumerator Restart(){
+    IEnumerator Restart(){
 
-		yield return new WaitForSeconds(respawnTime);
-
-		GameObject newPlayer = Instantiate (player, spawnPoint.position, Quaternion.Euler (new Vector3 (0, 0, 0)));
-		gameObject.GetComponent<UnityStandardAssets._2D.Camera2DFollow>().target = newPlayer.transform.GetChild(0);
+        player.SetActive(false);
+        GameObject.FindWithTag("MainCamera").GetComponent<AudioSource>().PlayOneShot(playerController.deathSound, playerController.deathVolume);
+        yield return new WaitForSeconds(respawnTime);       
+        playerController.currentHealth = playerController.maxHealth;
+        playerController.currentPower = 0;
+        playerController.heart1.gameObject.SetActive(true);
+        playerController.heart2.gameObject.SetActive(true);
+        playerController.heart3.gameObject.SetActive(true);
+        playerController.heart4.gameObject.SetActive(true);
+        playerController.energy1.gameObject.SetActive(false);
+        playerController.energy2.gameObject.SetActive(false);
+        playerController.energy3.gameObject.SetActive(false);
+        playerController.energy4.gameObject.SetActive(false);
+        playerController.energy5.gameObject.SetActive(false);
+        player.SetActive(true);
+        player.transform.position = spawnPoint.position;        
+		//GameObject newPlayer = Instantiate (player, spawnPoint.position, Quaternion.Euler (new Vector3 (0, 0, 0)));
+        //gameObject.GetComponent<UnityStandardAssets._2D.Camera2DFollow>().target = newPlayer.transform.GetChild(0);
 	}
 }
