c75136829950b78093f2c1b3b0e870037a8671b7
Jussi Inkeroinen <inkeroinenjussi@gmail.com>
2017-12-07 07:41:12 +0200

    Tried to make beavers to attack, didn't work

---
+++ Tree-Mendous/Assets/BeaverDamageArea.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeaverDamageArea : MonoBehaviour {
+
+	[SerializeField]
+	private BeaverController enemy;
+	float timer = 0;
+	float hitPeriod = 1f;
+
+	public AudioClip hitSound;
+	public float hitVolume = 0.7f;
+
+	void OnTriggerStay2D(Collider2D other){
+		if(other.tag == "Player"){
+			PlayerController player = other.gameObject.GetComponent<PlayerController>();
+
+
+			timer = timer + 1 * Time.deltaTime;
+
+			if(timer >= hitPeriod){
+				timer = 0;
+				player.addDamage (enemy.weaponDamage);
+				transform.root.GetComponent<AudioSource>().PlayOneShot (hitSound, hitVolume);
+			}
+		}
+	}
+
+	void OnTriggerExit2D(Collider2D other){
+		if(other.tag == "Player"){
+
+		}
+	}
+
+}
