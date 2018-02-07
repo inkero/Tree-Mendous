fbdd77b22cd3c08a2b6c66c5e191688188f0be5a
Jussi Inkeroinen <inkeroinenjussi@gmail.com>
2018-02-08 00:42:20 +0200

    Merge branch 'dev' of https://github.com/inkero/Tree-Mendous into dev

---
+++ Tree-Mendous/Assets/Scripts/SettingsMenuIngame.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class SettingsMenuIngame : MonoBehaviour {
+
+    public AudioMixer audioMixer;
+
+    public void SetMusicVolume(float volume)
+    {
+        audioMixer.SetFloat("musicvolume", volume);
+    }
+
+    public void SetSFXVolume(float volume)
+    {
+        audioMixer.SetFloat("sfxvolume", volume);
+    }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+    }
+}
