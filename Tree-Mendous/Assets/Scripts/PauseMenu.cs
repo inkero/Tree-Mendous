2f7ce4488546ce14fe649682c7ea86e52cec19f1
Frans <Frans.auranen@edu.turkuamk.fi>
2018-03-07 23:04:31 +0200

    Partial controller support

--- Tree-Mendous/Assets/Scripts/PauseMenu.cs
+++ Tree-Mendous/Assets/Scripts/PauseMenu.cs
@@ -10,7 +10,7 @@ public class PauseMenu : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
+        if (Input.GetKeyDown(KeyCode.Escape)||(Input.GetKeyDown(KeyCode.Joystick1Button7)))
         {
             if (GameIsPaused)
             {
